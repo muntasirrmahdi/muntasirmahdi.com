7988c4bda56d3ab7a8597869bedf586a43233ffc
muntasirrmahdi <muntasirrmahdi@users.noreply.github.com>
2026-06-10 01:58:23 +0600

    Update plan: Sanity CMS + Blog listing complete (Phase D+E)

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
+import { codeInput } from "@sanity/code-input";
 import { schemaTypes } from "./src/sanity/schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "pt5maea8";
@@ -12,7 +13,7 @@ export default defineConfig({
   projectId,
   dataset,
   basePath: "/studio",
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), codeInput()],
   schema: {
     types: schemaTypes,
   },
